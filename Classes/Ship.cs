db0742bedfe5c01a9511151abcf563b8963245d7
Gabriel Szałajko <maflex24@gmail.com>
2022-08-26 15:53:42 +0200

    Creating template

---
+++ Classes/Ship.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warship.Classes
+{
+    public class Ship
+    {
+        public string Name { get; private set; }
+        public int ShipLarge { get; private set; }
+        public List<Coordinate> Coordinates { get; set; }
+        public int DamagesAmount { get; private set; }
+        public bool Destroyed { get; private set; }
+
+        public Ship(string name, int shipLarge)
+        {
+            Name = name;
+            ShipLarge = shipLarge;
+        }
+
+        internal void PositionShip()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsOnTarget(Coordinate targetCoordinate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncreaseDamage() // +1 to DamagesAmount, and Destroyed = true if DamagesAmount == ShipLarge
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReportDamage() // Print info about damage, and destroyed if it's true
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Warship.Classes;
+
+var map = new Map(10, 10);
+
+var ships = new List<Ship>
+{
+    new Ship("Battleship", 5),
+    new Ship("Destroyer", 4),
+    new Ship("Destroyer", 4)
+};
+
+ships.ForEach(s => s.PositionShip());
+
+while (true)
+{
+    map.Show();
+
+    Coordinate targetCoordinate;
+
+    while (true)
+    {
+        Console.Write("Type your target: ");
+        targetCoordinate = Coordinate.CreateCoordinateFromInput(Console.ReadLine().ToUpper());
+
+        if (targetCoordinate is null)
+        {
+            Console.WriteLine("Your coordinate is wrong. Try again!");
+            continue;
+        }
+
+        if (!map.IsCoordinateExist(targetCoordinate))
+        {
+            Console.WriteLine("Coordinate doesn't exist");
+            continue;
+        }
+
+        var positionWasShootAt = map.WasCoordinateShooted(targetCoordinate);
+        if (positionWasShootAt)
+            Console.WriteLine("You were shooting on this position");
+        else
+            break;
+    }
+
+    var hitSuccessful = false;
+    foreach (var ship in ships)
+    {
+        var isOnTarget = ship.IsOnTarget(targetCoordinate);
+
+        if (isOnTarget)
+        {
+            hitSuccessful = true;
+            ship.IncreaseDamage();
+            ship.ReportDamage();
+        }
+    }
+
+    if (!hitSuccessful)
+    {
+        Console.WriteLine("You missed");
+        map.MarkShoot(targetCoordinate, false);
+        continue;
+    }
+
+    map.MarkShoot(targetCoordinate, true);
+
+    var allShipsDestroyed = ships.All(ship => ship.Destroyed);
+
+    if (allShipsDestroyed) break;
+}
+
+Console.WriteLine("Congratulations, you won!");
