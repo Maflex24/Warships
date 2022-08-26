79a9b629a76cb490bd9d3a1879cce802e0a40381
Gabriel Szałajko <maflex24@gmail.com>
2022-08-26 15:53:42 +0200

    Creating template

---
+++ Classes/Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warship.Classes
+{
+    public class Coordinate
+    {
+        public char X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinate(char x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Coordinate CreateCoordinateFromInput(string userInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Classes/Map.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace Warship.Classes
+{
+    public class Map
+    {
+        public Dictionary<char, string> Context { get; private set; }
+
+        public Map(int columns, int rows)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsCoordinateExist(Coordinate coordinate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WasCoordinateShooted(Coordinate coordinate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkShoot(Coordinate coordinate, bool successful)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
